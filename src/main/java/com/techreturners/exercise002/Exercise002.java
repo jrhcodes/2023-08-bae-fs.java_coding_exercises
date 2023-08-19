5e0efe9866c4fa2d5653b0796364b2c601c6c798
Julian <mrjrhirst@gmail.com>
2023-08-19 16:45:09 +0100

    Exercise 2 Finished - rough

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -1,15 +1,16 @@
 package com.techreturners.exercise002;
 
+import java.util.List;
+
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
+    public boolean isFromManchester( Person person ) {
         // Add your code here
-        return false;
+        return person.getCity().equals("Manchester");
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        // Add your code here
-        return false;
+    public boolean canWatchFilm(Person person, int ageLimit) {
+        return person.getAge() >= ageLimit;
     }
     
 }
